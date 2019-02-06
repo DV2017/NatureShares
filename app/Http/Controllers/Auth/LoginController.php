c657963655bc2c44fa907c750e13e49d6b29dbf2
DV <deepavarma.in@gmail.com>
2019-02-06 17:52:42 +0100

    Bringing authenticateUsers lgogin and logout functions to the LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -16,24 +19,80 @@ class LoginController extends Controller
     | redirecting them to your home screen. The controller uses a trait
     | to conveniently provide its functionality to your applications.
     |
-    */
+   */
 
-    use AuthenticatesUsers;
+  use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+  /**
+   * Where to redirect users after login.
+   *
+   * @var string
+   */
+  protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('guest')->except('logout');
+  }
+
+  /**
+   * Handle a login request to the application.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+   *
+   * @throws \Illuminate\Validation\ValidationException
+   */
+  public function login(Request $request)
+  {
+    $this->validateLogin($request);
+    if ($this->attemptLogin($request)) {
+      return $this->sendLoginResponse($request);
     }
+    return $this->sendFailedLoginResponse($request);
+  }
+
+
+  /**
+   * Send the response after the user was authenticated.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  protected function sendLoginResponse(Request $request)
+  {
+    $user = $this->guard()->user();
+    $user->generateToken();
+    return response()->json(['data' => $user->toArray(), 201]);
+  }
+
+
+  /**
+   * Log the user out of the application.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function logout(Request $request)
+  {
+    // $this->guard()->logout();
+
+    // $request->session()->invalidate();
+
+    // return $this->loggedOut($request) ? : redirect('/');
+    $user = Auth::guard('api')->user();
+
+    if ($user) {
+      $user->api_token = null;
+      $user->save();
+    }
+
+    return response()->json(['data' => 'User logged out.'], 200);
+
+  }
 }
